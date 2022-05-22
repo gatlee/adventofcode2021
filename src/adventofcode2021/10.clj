e7319955a91e255f5c31a98fbb0f880ce8fcdb59
Gatlee Kaw <gatlee.kaw@gmail.com>
2022-05-22 20:39:43 +1000

    Day 10 solution part 2

--- src/adventofcode2021/10.clj
+++ src/adventofcode2021/10.clj
@@ -14,7 +14,7 @@
    \[ \]
    \< \>})
 
-(def charToPoints
+(def invalidCharToPoints
   {\) 3
    \] 57
    \} 1197
@@ -26,25 +26,46 @@
          [currSymbol & rest] line]
 
     (cond
-      (nil? currSymbol) true
+      (nil? currSymbol) stack
       (openers->closers currSymbol) (recur (conj stack (openers->closers currSymbol)) rest)
       (= (peek stack) currSymbol) (recur (pop stack) rest)
       :else currSymbol)))
-
-
 
 (defn solvep1
   [input]
   (->> input
        parse-input
        (map validLine?)
-       (map charToPoints)
+       (map invalidCharToPoints)
        (filter some?)
        (reduce +)))
 
+(def completionCharToPoints
+  {\) 1
+   \] 2
+   \} 3
+   \> 4})
+
+(defn scoreCompletions
+  [list]
+  (->> (for [x (range (count list))]
+         (* (Math/pow 5 x) (completionCharToPoints (nth list x))))
+       (reduce + )
+       biginteger))
+
+(defn getMiddleScore
+  [list]
+  (nth list (/ (count list) 2)))
+
 (defn solvep2
   [input]
-  input)
+  (->> input
+       parse-input
+       (map validLine?)
+       (filter seqable?)
+       (map scoreCompletions)
+       sort
+       getMiddleScore))
 
 (solvep1 (slurp (io/resource "10-input.txt")))
 (solvep2 (slurp (io/resource "10-input.txt")))
